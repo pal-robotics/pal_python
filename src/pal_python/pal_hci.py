9470d1410f2251b80d27f8af4fc8814e3121d218
Paul Mathieu <paul.mathieu@pal-robotics.com>
2014-02-28 12:05:40 +0100

    pal_hci: add a REEM ear LED client

--- src/pal_python/pal_hci.py
+++ src/pal_python/pal_hci.py
@@ -3,13 +3,13 @@
 #
 # pal_python: pal_hci.py
 #
-# Copyright (c) 2013 PAL Robotics SL. All Rights Reserved
+# Copyright (c) 2013, 2014 PAL Robotics SL. All Rights Reserved
 #
 # Permission to use, copy, modify, and/or distribute this software for
 # any purpose with or without fee is hereby granted, provided that the
 # above copyright notice and this permission notice appear in all
 # copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
 # REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY
@@ -20,17 +20,96 @@
 #
 # Authors:
 #   * Siegfried-A. Gevatter
-
-# TODO: Rename to pal_sound?
+#   * Paul Mathieu
 
 import rospy
 import actionlib
 from actionlib_msgs.msg import GoalStatus
 
+import pal_device_msgs.srv as PDMS
+import pal_device_msgs.msg as PDM
+
+from .pal_common import configurable
+from .pal_rpc import AsyncServiceClient
+
+
+@configurable
+class Color(object):
+
+    defaults = {
+        'rgb': [0.0, 0.0, 0.0],
+        'blinking': False,
+    }
+
+    def __init__(self, cfg={}):
+        self._read_config(cfg)
+
+
+class ReemLedClient:
+    """
+    A simple client to access REEM's ear LEDs. Right now you will need one
+    client per color you want to use. Don't worry though, there will only
+    be one service client for everybody (per call type).
+
+    Intended use:
+
+        blue = Color({'rgb': [0.0, 0.0, 1.0]})
+        led_cl = ReemLedClient(blue)
+        led_cl.fire(1.0)  # will set the color of the ears to blue for 1 sec
+
+    """
+    _flat_color_client = None
+    _blink_color_client = None
+
+    def __init__(self, color):
+        self._color = color
+        if ReemLedClient._flat_color_client is None:
+            ReemLedClient._flat_color_client = AsyncServiceClient(
+                'ledManager/TimedColourEffect',
+                PDMS.TimedColourEffect,
+                persistent=True
+            )
+            ReemLedClient._blink_color_client = AsyncServiceClient(
+                'ledManager/TimedBlinkEffect',
+                PDMS.TimedColourEffect,
+                persistent=True
+            )
+
+    def fire(self, duration=1.0):
+        if self.color.blinking:
+            self._fire_blink(duration)
+        else:
+            self._fire_flat(duration)
+
+    def _fire_flat(self, duration):
+        effect = PDMS.TimedColourEffectRequest()
+        effect.leds.ledMask = PDM.LedGroup.LEFT_EAR | PDM.LedGroup.RIGHT_EAR
+        effect.effectDuration = rospy.Time(duration)
+        effect.priority = 96
+        effect.color.r = self._color.rgb[0]
+        effect.color.g = self._color.rgb[1]
+        effect.color.b = self._color.rgb[2]
+        self._flat_color_client.call(effect)
+
+    def _fire_blink(self, duration):
+        effect = PDMS.TimedBlinkEffectRequest()
+        effect.leds.ledMask = PDM.LedGroup.LEFT_EAR | PDM.LedGroup.RIGHT_EAR
+        effect.effectDuration = rospy.Time(duration)
+        effect.firstColorDuration = rospy.Time(0.2)
+        effect.secondColorDuration = rospy.Time(0.2)
+        effect.priority = 96
+        effect.firstColor.r = self._color.rgb[0]
+        effect.firstColor.g = self._color.rgb[1]
+        effect.firstColor.b = self._color.rgb[2]
+        # second color will be black by default
+        self._blink_color_client.call(effect)
+
+
 TTS_ACTION_NAME = '/sound'
 
 global __tts_client
 __tts_client = None
+
 
 # TODO: Add a non-blocking version?
 #       Possible design:
