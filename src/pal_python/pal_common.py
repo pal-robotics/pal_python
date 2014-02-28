fb931cf1624d76c997aa27393409894eb29838f8
Paul Mathieu <paul.mathieu@pal-robotics.com>
2014-02-28 12:04:32 +0100

    pal_common: add @configurable class decorator
    This helps setting configuration parameters to objects and
    default values. The doc is in the code.

--- src/pal_python/pal_common.py
+++ src/pal_python/pal_common.py
@@ -3,13 +3,13 @@
 #
 # pal_python: pal_common.py
 #
-# Copyright (c) 2013 PAL Robotics SL. All Rights Reserved
+# Copyright (c) 2013, 2014 PAL Robotics SL. All Rights Reserved
 #
 # Permission to use, copy, modify, and/or distribute this software for
 # any purpose with or without fee is hereby granted, provided that the
 # above copyright notice and this permission notice appear in all
 # copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
 # REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY
@@ -20,12 +20,53 @@
 #
 # Authors:
 #   * Siegfried-A. Gevatter
+#   * Paul Mathieu
 
 import xmlrpclib
 
 import rospy
 import rosgraph
 from roslib import scriptutil
+
+def configurable(cls):
+    """
+    This class decorator will add the _read_config method to any class.
+    _read_config() will take a struct and expand its contents into object
+    attributes. The primary use of this is in conjunction with parsed
+    config files or ROS parameters.
+
+    A class attribute struct 'defaults' can be used to set the value of
+    optional parameters.
+
+    Example:
+
+        @configurable
+        class Foo:
+            defaults = {  # 'foo' is not optional
+                'bar': 'hoge',
+                'opt': 42,
+            }
+            def __init__(self, cfg={}):
+                self._read_config(cfg)
+
+        # this will assign
+        #   f.foo = 37
+        #   f.bar = "baz"
+        #   f.opt = 42
+        f = Foo({'foo': 37, 'bar': 'baz'})
+
+    """
+    class Configurable(cls):
+        defaults = getattr(cls, 'defaults', {})
+
+        def _read_config(self, cfg):
+            for c in self.defaults:
+                if c in cfg:
+                    setattr(self, c, cfg[c])
+                else:
+                    setattr(self, c, self.defaults[c])
+    return Configurable
+
 
 def is_simulation():
     """
